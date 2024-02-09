fe0659d0ff416ff86f572fb681f0521c8f54a091
Malthe Gram <malthegram22@gmail.com>
2024-02-09 14:56:07 +0100

    frontpage wip

---
+++ src/lib/components/NextSection.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { ArrowBigSolid } from "flowbite-svelte-icons"
+	import { Tooltip } from "flowbite-svelte"
+	export let title
+	export let nextSectionId
+
+	const scrollToSection = () => {
+		const section = document.getElementById(nextSectionId)?.closest("section")
+		if (!section) return
+		section.scrollIntoView({ behavior: "smooth" })
+	}
+</script>
+
+<div class="flex flex-col items-center text-center mb-5">
+	<div class="flex items-center space-x-2">
+		<h1 id={title} class="text-3xl">{title}</h1>
+		<ArrowBigSolid on:click={scrollToSection} class="cursor-pointer" />
+		<Tooltip>Next section</Tooltip>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
+	import { Timeline, TimelineItem } from "flowbite-svelte"
 	import { onMount } from "svelte"
+	import NextSection from "$lib/components/NextSection.svelte"
 	const profilePicture =
 		"https://www.svgrepo.com/show/512721/profile-minus-1350.svg"
-	const notDesktop = true
 
 	let catchyText = ""
 	let isAlternateText = false
@@ -35,6 +36,93 @@
 	<p>{catchyText}</p>
 </div>
 
+<div>
+	<section class="bg-gray-400 section">
+		<div class="mt-10">
+			<NextSection nextSectionId={"section2"} title="My journey in Tech" />
+			<Timeline order="vertical">
+				<TimelineItem
+					title="Computer Science AP  @ KEA - Copenhagen School of Design and Technology"
+					date="Februray 2021 - June 2023"
+					><span>
+						I began my journey at <a target="_blank" href="https://www.kea.dk/"
+							>KEA - Copenhagen School of Design and Technology.</a
+						>
+					</span>
+				</TimelineItem>
+				<TimelineItem
+					title="Web Development @ Bonnier Publications"
+					date="Februray 2022 - January 2023"
+					><span>
+						I began as a part time web deveoper at <a
+							target="_blank"
+							href="https://www.bonnierpublications.com/"
+							>Bonnier Publications</a
+						>
+					</span>
+				</TimelineItem>
+				<TimelineItem
+					title="Internship @ Nets A/S"
+					date="January 2023 - April 2023"
+				>
+					<span>
+						I started my internship at <a
+							target="_blank"
+							href="https://www.nets.eu/dk-da">Nets</a
+						> where I worked on a test automation system alongside my colleague.
+					</span>
+				</TimelineItem>
+				<TimelineItem>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+					odit voluptates odio, aut numquam dolorem aliquam officiis enim
+					quisquam earum rerum inventore quo repudiandae quas doloribus tenetur
+					soluta possimus similique. Animi eligendi laborum perspiciatis sed
+					quod neque rerum delectus saepe ratione, itaque cupiditate soluta,
+					doloribus deleniti voluptas natus! Suscipit repudiandae quos vitae a
+					alias eveniet, quod dolore optio unde error.
+				</TimelineItem>
+				<TimelineItem
+					>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum vero
+					veniam eos libero porro dolorem quidem dignissimos molestiae nostrum
+					voluptate nemo sed, provident aspernatur adipisci magnam aliquid,
+					itaque accusamus ipsa!</TimelineItem
+				>
+				<TimelineItem
+					>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus,
+					modi distinctio? Laborum nesciunt perferendis, atque architecto
+					aliquam placeat aut provident consectetur omnis. Debitis adipisci
+					veritatis, voluptas nam praesentium dolores aspernatur?</TimelineItem
+				>
+				<TimelineItem
+					>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
+					temporibus error consequatur omnis ad velit quo corporis rerum, at
+					nostrum, eos quidem minima sunt rem sint tempora iusto inventore
+					aliquid.</TimelineItem
+				>
+				<TimelineItem
+					>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
+					temporibus error consequatur omnis ad velit quo corporis rerum, at
+					nostrum, eos quidem minima sunt rem sint tempora iusto inventore
+					aliquid.</TimelineItem
+				>
+			</Timeline>
+		</div>
+	</section>
+	<section class="bg-gray-400 section">
+		<div class="mt-10">
+			<NextSection title="section2" />
+			<Timeline order="vertical">
+				<TimelineItem
+					>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi
+					adipisci sequi temporibus iste! Placeat possimus quo rerum sequi non
+					cum ullam dolore neque dolorum! Doloribus illum eveniet sapiente
+					architecto voluptas.</TimelineItem
+				>
+			</Timeline>
+		</div>
+	</section>
+</div>
+
 <style lang="scss">
 	.main {
 		display: flex;
@@ -52,6 +140,29 @@
 			position: absolute;
 			margin-top: 15rem;
 			font-size: 2rem;
+		}
+	}
+	.section {
+		min-height: 100vh;
+		display: flex;
+		justify-content: center;
+
+		div {
+			max-width: 50%;
+
+			a {
+				&:hover {
+					text-decoration: underline;
+					color: #595959;
+				}
+				&:active {
+					color: #4a4a4a;
+				}
+			}
+		}
+
+		&:nth-child(odd) {
+			background-color: #ecf0f1;
 		}
 	}
 </style>
